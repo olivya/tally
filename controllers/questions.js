8a630bd8626eca4d3a49b12352a7bce5fd8aed46
olivya <s.olivya@gmail.com>
2015-02-08 16:15:15 -0800

    fixed question form validation (hopefully), adjusted question form styling

--- controllers/questions.js
+++ controllers/questions.js
@@ -18,32 +18,49 @@ tally.controller('questionsController', function($scope, QuestionBank) {
 	$scope.message = 'Tally is a customizable trivia app.';
 
 	$scope.addQuestion = function(){
-		$scope.questionBank.push(
-			{
-				question:$scope.newQuestion,
-				//populate options:
-				options:[ $scope.newOptionA,
-						  $scope.newOptionB,
-						  $scope.newOptionC,
-						  $scope.newOptionD ],
-				answer:$scope.setAnswer
-			});
+		// console.log($scope.addQuestionForm);
+		// console.log("test");
+		// if($scope.addQuestionForm.$valid) {
+			$scope.questionBank.push(
+				{
+					question:$scope.newQuestion,
+					//populate options:
+					options:[ $scope.newOptionA,
+							  $scope.newOptionB,
+							  $scope.newOptionC,
+							  $scope.newOptionD ],
+					answer:$scope.setAnswer
+				});
 
-		$scope.newQuestion = "";
-		$scope.newOptionA = "";
-		$scope.newOptionB = "";
-		$scope.newOptionC = "";
-		$scope.newOptionD = "";
-		$scope.setAnswer = "";
-		console.log($scope.questionBank.length);
+			$scope.newQuestion = "";
+			$scope.newOptionA = "";
+			$scope.newOptionB = "";
+			$scope.newOptionC = "";
+			$scope.newOptionD = "";
+			$scope.setAnswer = "";
 
-		if ($scope.questionBank.length===0) {
-  			$scope.noQuestions = true;
-  		}
-  		else if ($scope.questionBank.length >= 1) {
-  			$scope.noQuestions = false;
-  		}
+			if ($scope.questionBank.length===0) {
+					$scope.noQuestions = true;
+				}
+			else if ($scope.questionBank.length >= 1) {
+					$scope.noQuestions = false;
+				}
+			// }
+		// else {
+		// 	alert("Please complete all fields");
+		// }
 	};
+
+	// $scope.errorMessage = function(){
+	// 	console.log($scope.addQuestionForm.$invalid);
+
+	// 	if($scope.addQuestionForm.$invalid) {
+	// 		$scope.errorMessageTriggered = true;
+	// 	}
+	// 	else if($scope.addQuestionForm.$valid) {
+	// 		$scope.errorMessageTriggered = false;
+	// 	}
+	// };
 
 	 $scope.deleteQuestion = function (i){ // i = $index from home.html
     	$scope.questionBank.splice(i,1);
